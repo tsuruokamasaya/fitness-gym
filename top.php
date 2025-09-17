2e27a42b5743176fefa1f958f8a915e391a8f0b9
tsuruokamasaya <masayaiueo@gmail.com>
2025-09-17 11:28:04 +0900

    index.htmlを、新しいtop.phpにコピー

---
+++ top.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fitness-gym</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c&display=swap" rel="stylesheet">
+    <script src="script.js" defer></script>
+</head>
+
+<body>
+    <header class="header">
+        <h1>Fitness-gym</h1>
+        <ul class="header-menu">
+            <li><a href="#">about</a></li>
+            <li><a href="#">service</a></li>
+            <li><a href="#">information</a></li>
+            <li><a href="#">ご入会方法</a></li>
+            <li><a href="#">SNS</a></li>
+            <li><a href="#">お問い合わせ</a></li>
+        </ul>
+    </header>
+
+    <main>
+        <section id="about">
+            <h2>Fitness-gymはこんなところ</h2>
+            <p>本格的にトレーニングしたい方はもちろん、今までジムに通ったことがない方、</p>
+            <p>家族みんなで通いたい方、どなたでも気軽にいつでも始められます。</p>
+
+            <ul id="about_grid">
+                <li>
+                    <figure>
+                        <img src="https://dummy.kobeya.com/?width=300&height=170&bg=cccccc&color=000000&text=%E3%82%B5%E3%83%B3%E3%83%97%E3%83%AB%E7%94%BB%E5%83%8F&_=1757992751403" alt="サンプル画像（300×170）">
+                        <figcaption>年中無休</figcaption>
+                    </figure>
+                </li>
+
+                <li>
+                    <figure>
+                        <img src="https://dummy.kobeya.com/?width=300&height=170&bg=cccccc&color=000000&text=%E3%82%B5%E3%83%B3%E3%83%97%E3%83%AB%E7%94%BB%E5%83%8F&_=1757992751403" alt="サンプル画像（300×170）">
+                        <figcaption>家族割</figcaption>
+                    </figure>
+                </li>
+
+                <li>
+                    <figure>
+                        <img src="https://dummy.kobeya.com/?width=300&height=170&bg=cccccc&color=000000&text=%E3%82%B5%E3%83%B3%E3%83%97%E3%83%AB%E7%94%BB%E5%83%8F&_=1757992751403" alt="サンプル画像（300×170）">
+                        <figcaption>こだわりマシン</figcaption>
+                    </figure>
+                </li>
+
+                <li>
+                    <figure>
+                        <img src="https://dummy.kobeya.com/?width=300&height=170&bg=cccccc&color=000000&text=%E3%82%B5%E3%83%B3%E3%83%97%E3%83%AB%E7%94%BB%E5%83%8F&_=1757992751403" alt="サンプル画像（300×170）">
+                        <figcaption>レディースエリア</figcaption>
+                    </figure>
+                </li>
+
+                <li>
+                    <figure>
+                        <img src="https://dummy.kobeya.com/?width=300&height=170&bg=cccccc&color=000000&text=%E3%82%B5%E3%83%B3%E3%83%97%E3%83%AB%E7%94%BB%E5%83%8F&_=1757992751403" alt="サンプル画像（300×170）">
+                        <figcaption>ビジター、体験あり</figcaption>
+                    </figure>
+                </li>
+
+                <li>
+                    <figure>
+                        <img src="https://dummy.kobeya.com/?width=300&height=170&bg=cccccc&color=000000&text=%E3%82%B5%E3%83%B3%E3%83%97%E3%83%AB%E7%94%BB%E5%83%8F&_=1757992751403" alt="サンプル画像（300×170）">
+                        <figcaption>ポイント制度</figcaption>
+                    </figure>
+                </li>
+            </ul>
+        </section>
+
+        <section id="service">
+            <h2>施設のご案内</h2>
+            <p>広々とした施設内にフリーウエイト、筋トレマシン、有酸素マシンなど本格的マシンを豊富にラインナップ。</p>
+            <p>フリースペースエリアでは、ストレッチ、体幹などを鍛えるトレーニングも。</p>
+
+            <div id="gym_image">
+                <img src="https://dummy.kobeya.com/?width=1000&height=450&bg=cccccc&color=000000&text=%E3%82%B5%E3%83%B3%E3%83%97%E3%83%AB%E7%94%BB%E5%83%8F&_=1757992751403" alt="サンプル画像（1000×450" alt="ジムの全体イメージ画像">
+                <p><button>施設紹介を詳しく見る</button></p>
+            </div>
+
+            <h2>家族会員サービス</h2>
+
+            <p>家族4人で使った場合、1名様分の月会費が745円（税込819円）※月会費が2,980円（税込3,278円）の場合ご入会1名につき、家族3名まで無料でFIT365の会員登録ができるサービスです。</p>
+            <p>ご本人はもちろん、ご登録いただいたご家族分の会員カードも発行されるので、離れて暮らすご家族でも、いつでも気軽にご利用いただけます</p>
+
+            <ul id="family_service">
+                <li><img src="https://dummy.kobeya.com/?width=300&height=400&bg=cccccc&color=000000&text=%E3%82%B5%E3%83%B3%E3%83%97%E3%83%AB%E7%94%BB%E5%83%8F&_=1757992751403" alt="特典1（300×170）"></li>
+                <li><img src="https://dummy.kobeya.com/?width=300&height=400&bg=cccccc&color=000000&text=%E3%82%B5%E3%83%B3%E3%83%97%E3%83%AB%E7%94%BB%E5%83%8F&_=1757992751403" alt="特典2（300×170）"></li>
+                <li><img src="https://dummy.kobeya.com/?width=300&height=400&bg=cccccc&color=000000&text=%E3%82%B5%E3%83%B3%E3%83%97%E3%83%AB%E7%94%BB%E5%83%8F&_=1757992751403" alt="特典3（300×170）"></li>
+                <li><img src="https://dummy.kobeya.com/?width=300&height=400&bg=cccccc&color=000000&text=%E3%82%B5%E3%83%B3%E3%83%97%E3%83%AB%E7%94%BB%E5%83%8F&_=1757992751403" alt="特44（300×170）"></li>
+            </ul>
+        </section>
+        <section id="information">
+            <h2>ご入会方法</h2>
+            <h3>WEBまたはアプリからお手続き可能です</h3>
+            <button id="information_button">入会方法を詳しく見る</button>
+
+            <h2>ソーシャルメディア公式アカウント</h2>
+            <div id="information-flex">
+                <div>
+                    <img src="https://dummy.kobeya.com/?width=300&height=200&bg=cccccc&color=000000&text=%E3%82%B5%E3%83%B3%E3%83%97%E3%83%AB%E7%94%BB%E5%83%8F&_=1757992751403" alt="特典1（300×170）">
+                    <p>記事1</p>
+                </div>
+
+                <div>
+                    <img src="https://dummy.kobeya.com/?width=300&height=200&bg=cccccc&color=000000&text=%E3%82%B5%E3%83%B3%E3%83%97%E3%83%AB%E7%94%BB%E5%83%8F&_=1757992751403" alt="特典1（300×170）">
+                    <p>記事2</p>
+                </div>
+                <div>
+                    <img src="https://dummy.kobeya.com/?width=300&height=200&bg=cccccc&color=000000&text=%E3%82%B5%E3%83%B3%E3%83%97%E3%83%AB%E7%94%BB%E5%83%8F&_=1757992751403" alt="特典1（300×170）">
+                    <p>記事3</p>
+                </div>
+
+            </div>
+        </section>
+    </main>
+
+    <footer id="footer">
+        <h2>Fitness-gym</h2>
+        <div class="sns-icon">
+            <img src="" alt="">
+            <img src="" alt="">
+            <img src="" alt="">
+        </div>
+
+        <div class="copyright">Copyright © 2025 Fitness-gym. All Rights Reserved.</div>
+        </p>
+    </footer>
+</body>
+
+</html>
