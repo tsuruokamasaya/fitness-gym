fc287ba57f72c91c85097477b08f99d150d760d3
tsuruokamasaya <masayaiueo@gmail.com>
2025-09-24 11:26:39 +0900

    会員登録サービスビルド

--- register.php
+++ register.php
@@ -2,23 +2,26 @@
 include 'includes/common.php';
 include 'includes/header.php'; ?>
 
+
 <div>
-    <form action="register_done.php">
+    <h1>新規会員登録</h1>
+    <a href="login.php">すでに会員登録済みの方はこちら</a>
+    <form action="register_done.php" method="post">
         <p>
             <label for="username">
-                <input type="text" id="username" placeholder="例）山田　太郎">
+                <input type="text" id="username" placeholder="例）山田　太郎" required>
             </label>
         </p>
 
         <p>
             <label for="email">
-                <input type="text" id="email" placeholder="例）example@email.com">
+                <input type="text" id="email" placeholder="例）example@email.com" required>
             </label>
         </p>
 
         <p>
             <label for="password">
-                <input type="password" id="password">
+                <input type="password" id="password" required>
             </label>
         </p>
 
